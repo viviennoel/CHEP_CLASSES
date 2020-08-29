4b899c698588781ba89c748ab5e6305e2b6f990b
viviennoel <52512132+viviennoel@users.noreply.github.com>
2020-08-29 15:53:43 +0200

    Upload /api file

---
+++ api/controllers/Simulator.js
@@ -0,0 +1,25 @@
+﻿const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const Dresses = require('../models/Dresses.js');
+
+//SAVE THE DRESSES IN THE DATABASE
+
+exports.simulatorDresses = (req, res, next) => {
+    const dress = new Dresses({
+        userId: req.body.userId,
+        datenow: req.body.datenow,
+        shoulder: req.body.shoulder,
+        style: req.body.style,
+        back: req.body.back,
+        color: req.body.color,
+        fabric: req.body.fabric
+    });
+
+    console.log(dress);
+
+    dress.save()
+        .then(() => res.status(201).json({ message :'succès dress saved'
+        }))
+
+};
+
