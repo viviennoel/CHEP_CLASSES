1ed89629fa2507fed8535ddf2923abb9e988460f
viviennoel <52512132+viviennoel@users.noreply.github.com>
2020-08-29 16:09:33 +0200

    BACKEND

---
+++ models/Dresses.js
@@ -0,0 +1,15 @@
+﻿const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const dressesSchema = mongoose.Schema({
+    userId: { type: String, required: true },
+    datenow: { type: Number, required: true },
+    shoulder: { type: String, required: true },
+    style: { type: String, required: true },
+    back: { type: String, required: true },
+    color: { type: String, required: true },
+    fabric: { type: String, required: true },
+})
+
+
+module.exports = mongoose.model('Dresses', dressesSchema);
