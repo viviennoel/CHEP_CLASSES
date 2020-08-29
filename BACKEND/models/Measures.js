009a652c27df509d0f6ae9b9202cc462c92aeb27
viviennoel <52512132+viviennoel@users.noreply.github.com>
2020-08-29 14:51:15 +0200

    BACKEND

---
+++ BACKEND/models/Measures.js
@@ -0,0 +1,25 @@
+﻿const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const measuresSchema = mongoose.Schema({
+    userId: { type: String, required: true },
+    datenow: { type: Number, required: true },
+    unity: { type: String, required: true },
+    measure1: { type: String, required: true  },
+    measure2: { type: String, required: true  },
+    measure3: { type: String, required: true  },
+    measure4: { type: String, required: true  },
+    measure5: { type: String, required: true  },
+    measure6: { type: String, required: true  },
+    measure7: { type: String, required: true  },
+    measure8: { type: String, required: true  },
+    measure9: { type: String, required: true  },
+    measure10: { type: String, required: true  },
+    measure11: { type: String, required: true  },
+    measure12: { type: String, required: true  },
+    measure13: { type: String, required: true  },
+    measure14: { type: String, required: true },
+    measure15: { type: String, required: true },
+})
+
+module.exports = mongoose.model('Measures', measuresSchema);
---
+++ BACKEND/routes/Simulator.js
@@ -0,0 +1,11 @@
+﻿const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../controllers/Simulator.js');
+const auth = require('../middleware/auth');
+
+//ENREGISTRER LES ROBES DU SIMULATEUR
+
+router.post('/dresses', auth, usersCtrl.simulatorDresses)
+
+
+module.exports = router;
